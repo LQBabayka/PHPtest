5e0b795aa4176b108a5e998fc1cba8b5afc584a5
LQBabayka <timashevalexandr@gmail.com>
2022-11-13 22:53:49 +0400

    sql start2

--- mysql/DB_dump.sql
+++ mysql/DB_dump.sql
@@ -16,21 +16,44 @@
 CREATE DATABASE IF NOT EXISTS `test_base` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `test_base`;
 
+-- Дамп структуры для таблица test_base.classics
+CREATE TABLE IF NOT EXISTS `classics` (
+  `author` varchar(128) DEFAULT NULL,
+  `title` varchar(128) DEFAULT NULL,
+  `category` varchar(16) DEFAULT NULL,
+  `year` smallint DEFAULT NULL,
+  `isbn` char(13) NOT NULL,
+  PRIMARY KEY (`isbn`),
+  KEY `author` (`author`(20)),
+  KEY `title` (`title`(20)),
+  KEY `category` (`category`(4)),
+  KEY `year` (`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Дамп данных таблицы test_base.classics: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `classics` DISABLE KEYS */;
+INSERT INTO `classics` (`author`, `title`, `category`, `year`, `isbn`) VALUES
+	('dfdfgdfg', 'dfgdfgdfg', 'dfgdfg', 1999, '199987743');
+/*!40000 ALTER TABLE `classics` ENABLE KEYS */;
+
 -- Дамп структуры для таблица test_base.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fullname` varchar(50) DEFAULT NULL,
   `login` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
+  `deleted` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Дамп данных таблицы test_base.users: ~0 rows (приблизительно)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `fullname`, `login`, `password`) VALUES
-	(1, 'Тимашев Александр Алексеевич', 'admin', 'admin'),
-	(2, 'Криворогов Антон Степанович', 'start', 'full'),
-	(3, 'Елисеева Светлана Андреевна', 'elis', 'dron');
+INSERT INTO `users` (`id`, `fullname`, `login`, `password`, `deleted`) VALUES
+	(1, 'Тимашев Александр Алексеевич', 'admin', 'admin', 0),
+	(2, 'Криворогов Антон Степанович', 'start', 'full', 0),
+	(3, 'Елисеева Светлана Андреевна', 'elis', 'dron', 0),
+	(4, 'dfgdf', 'erger', 'fdgdfg', 0),
+	(5, 'fgdfg', 't3weqa', 'fg54', 0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
